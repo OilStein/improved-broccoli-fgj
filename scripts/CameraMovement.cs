91615cd590020bb72403bd826f7d41dff78c7de7
Niko Sihvo <nikobranart@gmail.com>
2023-02-04 13:47:43 +0200

    viewport. Not working methods

--- scripts/CameraMovement.cs
+++ scripts/CameraMovement.cs
@@ -6,13 +6,20 @@ public class CameraMovement : Node2D
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
+	private Viewport viewport;
 	
 	private Camera2D camera;
 	private Vector2 yLocation = new Vector2();
+	
+	
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// Error here
+		viewport.SetSizeOverride(true, Vector2(720, 2680));
+		viewport.SetSizeOverrideStretch(true);
+		
 		camera = GetNode<Camera2D>("Camera2D");
 		var cPosition = camera.GetCameraPosition();
 		GD.Print(cPosition);
