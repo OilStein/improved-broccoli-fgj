ad383a5574d1c1571bed8618abde61003aa46e53
Arttu Hynninen <ar2.hynninen@gmail.com>
2023-02-04 21:55:54 +0200

    Fixing merge

--- Scripts/ScoreLabel.cs
+++ Scripts/ScoreLabel.cs
@@ -3,22 +3,22 @@ using System;
 
 namespace Beatroot
 {
-    public class ScoreLabel : Node2D
-    {
-        private Label _scoreLabel;
+	public class ScoreLabel : Node2D
+	{
+		private Label _scoreLabel;
 
-        public override void _Ready()
-        {
-            _scoreLabel = GetChild<Label>(0);
-            _scoreLabel.Text = "Score: 0";
+		public override void _Ready()
+		{
+			_scoreLabel = GetChild<Label>(0);
+			_scoreLabel.Text = "Score: 0";
 
-            var global = GetNode<Global>("/root/Global");
-            global.Connect("ScoreChanged", this, "UpdateLabel");
-        }
+			var global = GetNode<Global>("/root/Global");
+			global.Connect("ScoreChanged", this, "UpdateLabel");
+		}
 
-        public void UpdateLabel(int score)
-        {
-            _scoreLabel.Text = $"Score: {score}";
-        }
-    }
+		public void UpdateLabel(int score)
+		{
+			_scoreLabel.Text = $"Score: {score}";
+		}
+	}
 }
