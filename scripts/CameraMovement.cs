ec9916cbeef8202b119e6ada942ae5a4397e4476
Niko Sihvo <nikobranart@gmail.com>
2023-02-04 13:32:04 +0200

    Gamescreen + camera

---
+++ scripts/CameraMovement.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public class CameraMovement : Node2D
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+	
+	private Camera2D camera;
+	private Vector2 yLocation = new Vector2();
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		camera = GetNode<Camera2D>("Camera2D");
+		var cPosition = camera.GetCameraPosition();
+		GD.Print(cPosition);
+	}
+	
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		// Impl. state boolean day
+		if (Input.IsActionPressed("ui_down"))
+		{
+			yLocation.y += 10;
+			camera.SetGlobalPosition(yLocation);
+		}
+		
+		// Impl. state boolean night
+		if (Input.IsActionPressed("ui_up"))
+		{
+			yLocation.y -= 10;
+			camera.SetGlobalPosition(yLocation);
+		}
+		
+ 	}
+}
